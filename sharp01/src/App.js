9e62169e735b0db1e20067296937eaf6b7285865
Nilesh Ranpise <78468875+NileshRanpise@users.noreply.github.com>
2022-04-09 18:08:30 +0530

    1st componant

---
+++ sharp01/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1> Expense Item </h1>
+   <h2>Food Rs 10</h2>
+   <h2>Petrol Rs 100</h2>
+   <h2>Movies Rs 200</h2>
+    </div>
+  );
+}
+
+export default App;
