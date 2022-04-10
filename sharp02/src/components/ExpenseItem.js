2e79a08f46b463cde1f3de489d96954dd106ca0c
Nilesh Ranpise <78468875+NileshRanpise@users.noreply.github.com>
2022-04-10 13:38:43 +0530

    State topic

--- sharp02/src/components/ExpenseItem.js
+++ sharp02/src/components/ExpenseItem.js
@@ -1,13 +1,23 @@
+import React, { useState } from 'react';
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   
+  const [title, setTitle] = useState(props.title);
+  // console.log('ExpenseItem evaluated by React');
+  const [amount, setAmout] = useState(props.amount);
+
   const clickHandler = () => {
-    const exp = document.getElementById('exp');
-    exp.remove();
-    console.log("Deleted Item !!!!");
+    
+    setTitle("Updated!!!");
+    setAmout(100);
+    console.log(title);
+  
+    // const exp = document.getElementById('exp');
+    // exp.remove();
+    // console.log("Deleted Item !!!!");
   };
 
   return (
@@ -15,11 +25,12 @@ function ExpenseItem(props) {
       <Card className="expense-item" >
         <ExpenseDate date={props.date} />
         <div className="expense-item__description">
-          <h2 id="exp">{props.title} </h2>
+          <h2>{title} </h2>
           <div className="expense-item__price">${props.amount}</div>
         </div>
         {/* <button onClick={clickHandler}> Change Title </button> */}
-        <button onClick={clickHandler}> Delete </button>
+        {/* <button onClick={clickHandler}> Delete </button> */}
+        <button onClick={clickHandler}> Update </button>
       </Card>
     
   );
